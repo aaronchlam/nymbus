2320b201e622a9d098640941bfca6b5f29706529
aaronchlam <aaron.chihon.lam@gmail.com>
2014-10-04 06:43:02 -0400

    test

--- nymbus.py
+++ nymbus.py
@@ -1,14 +1,18 @@
-import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 handshakes = []
-profiles = {'12345': 'www.nymion.herokuapp.com'}
+profiles = {}
+recent_handshakes = []
 
 @app.route('/')
 def index():
     return "Hello World"
+
+@app.route('/recent-handshakes')
+def recent_handshakes:
+
 
 @app.route('/new-handshake', methods = ['POST'])
 def new_handshake():
@@ -24,19 +28,20 @@ def get_handshake():
     handshake = find_profile(request.json['handshake'])
     if handshake == 0:
         return jsonify({'status': 'not-ok'})
-    profile_url = profiles[handshake['nymi_id']]
+    profile = profiles[handshake['nymi_id']]
+    
     return jsonify({'status': 'ok',
-                    'profile': profile_url})
+                    'profile': profile['url']})
     
 def find_profile(handshake):
     handshake_ts = long(handshake['timestamp'])
     for hs in handshakes:
         if hs['nymi_id'] == handshake['nymi_id']:
-            return 0
+            continue
         time_diff = handshake_ts - long(hs['timestamp'])
-        if abs(time_diff) < 5000:
+        if abs(time_diff) < 100:
             return hs
     return 0
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
